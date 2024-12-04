4cbf6b365195577f942a5f1b561825941a3cd1a8
TropicSapling <tropical.productions.scratch@gmail.com>
2024-12-04 22:57:44 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,17 @@ mod helpers;
 use std::error::Error;
 use data::*;
 
+/// TODO: PARALLELISM - bulk data processing
+/// 
+/// For every neuron *in parallel*, sequentially:
+/// 1. Initiate *local* `neuron_excitations = vec![0; neurons.len()]`
+/// 2. Check if neuron should be activated
+/// 3. If so, for each connection: `neuron_excitations[recv_neuron] += conn.charge`
+/// 
+/// Then, once every neuron has finished, sequentially:
+/// 1. Collect all `neuron_excitations` and sum them together into `total_excitations`
+/// 2. Assign `total_excitations[neuron]` to each corresponding neuron *in parallel*
+
 fn main() -> Result<(), Box<dyn Error>> {
 	println!("");
 
@@ -20,6 +31,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 	
 	// Train agents on the input data
 	trainer::train(&mut agents, input_data, 53248);
+	// note: currently there is practically no improvement beyond 53248*2=106496
 
 	// Print final top agent
 	debug::result(&mut agents[0], input_data);
